dea03a1f3f0a2e7fa438dfa46edcb55172fe9e61
Mou Lai <geeklai@foxmail.com>
2022-10-07 12:16:12 +0800

    Add function: check-md5-only

--- download_NEX-GDDP-CMIP6.py
+++ download_NEX-GDDP-CMIP6.py
@@ -4,81 +4,7 @@
 
 import os
 import pandas as pd
-
-# Read in the csv "gddp-cmip6-thredds-fileserver.csv" which contains the NEX-GDDP-CMIP6 urls
-file_list = pd.read_csv('gddp-cmip6-files.csv')
-
-# Let the user choose the Model, Experiment, and Variable to be downloaded
-# Print the available Models
-print('Available Models:')
-print(file_list['Model'].unique())
-print('Use commas to connect different model names, or enter "all" to indicate all models')
-model = input('Model: ')
-if model == 'all':
-    model = file_list['Model'].unique()
-else:
-    model = model.split(',')
-# Print the available Experiments
-print('Available Experiments:')
-print(file_list['Experiment'].unique())
-print('Use commas to connect different experiment names, or enter "all" to indicate all experiments')
-experiment = input('Experiment: ')
-if experiment == 'all':
-    experiment = file_list['Experiment'].unique()
-else:
-    experiment = experiment.split(',')
-# Print the available Variables
-print('Available Variables:')
-print(file_list['Variable'].unique())
-print('Use commas to connect different variable names, or enter "all" to indicate all variables')
-variable = input('Variable: ')
-if variable == 'all':
-    variable = file_list['Variable'].unique()
-else:
-    variable = variable.split(',')
-
-# Remove the spaces in the names
-model = [x.strip() for x in model]
-experiment = [x.strip() for x in experiment]
-variable = [x.strip() for x in variable]
-
-# Filter the file_list based on the user's choices
-file_list = file_list[file_list['Model'].isin(model)]
-file_list = file_list[file_list['Experiment'].isin(experiment)]
-file_list = file_list[file_list['Variable'].isin(variable)]
-# Reset the index
-file_list = file_list.reset_index(drop=True)
-
-# Print the user's choices
-print('='*50)
-print('Please confirm your choices:')
-print('Model: ', model)
-print('Experiment: ', experiment)
-print('Variable: ', variable)
-print('Total number of files to be downloaded: {}'.format(len(file_list)))
-
-print('Do you want to check the MD5 checksums? (y/n) (Default: y)')
-md5 = input('MD5 check: ')
-if md5.strip() == 'n':
-    md5 = False
-else:
-    md5 = True
-
-print('If the file already exists, do you want to overwrite it? (y/n) (Default: n)')
-overwrite = input('Overwrite: ')
-if overwrite.strip() == 'y':
-    overwrite = True
-else:
-    overwrite = False
-
-print('Input the path to save the files. (Defalut: ./NEX-GDDP-CMIP6)')
-root_path = input('Path: ')
-if root_path == '':
-    root_path = os.path.abspath('./NEX-GDDP-CMIP6')
-else:
-    root_path = os.path.abspath(root_path)
-
-print('='*50)
+import sys
 
 def md5sum(file_path):
     '''
@@ -89,6 +15,33 @@ def md5sum(file_path):
         data = f.read()
         md5 = hashlib.md5(data).hexdigest()
     return md5
+
+def md5_check_single(file_path, file_md5):
+    '''
+    Check the MD5 checksum of the file
+    '''
+    # Check if the file exists
+    if not os.path.exists(file_path):
+        print('The file {} does not exist.'.format(file_path))
+        return
+    if file_md5 == md5sum(file_path):
+        print('The MD5 checksum of {} is correct.'.format(file_path))
+    else:
+        print('The MD5 checksum of {} is incorrect.'.format(file_path))
+        print('The downloaded file is removed.')
+        os.remove(file_path)
+
+def md5_check_remove(file_path_list, md5_list):
+    """
+    Check the MD5 checksum of the files using joblib
+    If the MD5 is incorrect, the file will be removed.
+
+    Args:
+        file_path_list (list): A list of file paths
+        md5_list (list): A list of MD5 checksums
+    """
+    from joblib import Parallel, delayed
+    Parallel(n_jobs=-1)(delayed(md5_check_single)(file_path, file_md5) for file_path, file_md5 in zip(file_path_list, md5_list))
 
 def download_file(url, path, file_md5, md5_check, overwrite):
     '''
@@ -148,16 +101,106 @@ def download_file(url, path, file_md5, md5_check, overwrite):
             return
     print('The file is downloaded to {}'.format(path))
 
-# Download the files in the path "./Model/Experiment/Variable/..."
-for index, row in file_list.iterrows():
-    # Create the path
-    path = os.path.join(root_path, row['Model'], row['Experiment'], row['Variable'])
-    # Create the path if it doesn't exist
-    if not os.path.exists(path):
-        os.makedirs(path)
-    # Download the file
-    file_md5 = row['fileMD5']
-    download_file(row['fileUrl'], path, file_md5=file_md5, md5_check=md5, overwrite=overwrite)
-    # Print the progress
-    print('Processed ' + str(index+1) + ' of ' + str(len(file_list)) + ' files')
-    print()
+if __name__ == '__main__':
+    # Read in the csv "gddp-cmip6-thredds-fileserver.csv" which contains the NEX-GDDP-CMIP6 urls
+    file_list = pd.read_csv('gddp-cmip6-files.csv')
+
+    # Let the user choose the Model, Experiment, and Variable to be downloaded
+    # Print the available Models
+    print('Available Models:')
+    print(file_list['Model'].unique())
+    print('Use commas to connect different model names, or enter "all" to indicate all models')
+    model = input('Model: ')
+    if model == 'all':
+        model = file_list['Model'].unique()
+    else:
+        model = model.split(',')
+    # Print the available Experiments
+    print('Available Experiments:')
+    print(file_list['Experiment'].unique())
+    print('Use commas to connect different experiment names, or enter "all" to indicate all experiments')
+    experiment = input('Experiment: ')
+    if experiment == 'all':
+        experiment = file_list['Experiment'].unique()
+    else:
+        experiment = experiment.split(',')
+    # Print the available Variables
+    print('Available Variables:')
+    print(file_list['Variable'].unique())
+    print('Use commas to connect different variable names, or enter "all" to indicate all variables')
+    variable = input('Variable: ')
+    if variable == 'all':
+        variable = file_list['Variable'].unique()
+    else:
+        variable = variable.split(',')
+
+    # Remove the spaces in the names
+    model = [x.strip() for x in model]
+    experiment = [x.strip() for x in experiment]
+    variable = [x.strip() for x in variable]
+
+    # Filter the file_list based on the user's choices
+    file_list = file_list[file_list['Model'].isin(model)]
+    file_list = file_list[file_list['Experiment'].isin(experiment)]
+    file_list = file_list[file_list['Variable'].isin(variable)]
+    # Reset the index
+    file_list = file_list.reset_index(drop=True)
+
+    # Print the user's choices
+    print('='*50)
+    print('Please confirm your choices:')
+    print('Model: ', model)
+    print('Experiment: ', experiment)
+    print('Variable: ', variable)
+    print('Total number of files to be downloaded: {}'.format(len(file_list)))
+
+    print('Input the path to save the files. (Defalut: ./NEX-GDDP-CMIP6)')
+    root_path = input('Path: ')
+    if root_path == '':
+        root_path = os.path.abspath('./NEX-GDDP-CMIP6')
+    else:
+        root_path = os.path.abspath(root_path)
+    
+    # If argv = "--Check-md5-only", then only check the MD5 checksum
+    if len(sys.argv) > 1:
+        if '--check-md5-only' in sys.argv:
+            print('Only check the MD5 checksum.')
+            # file_path format: "root_path/Model/Experiment/Variable/..."
+            file_path_list = [os.path.join(root_path,
+                file_list['Model'][i],
+                file_list['Experiment'][i],
+                file_list['Variable'][i],
+                file_list['fileUrl'][i].split('/')[-1]) for i in range(len(file_list))]
+            md5_list = file_list['fileMD5'].tolist()
+            md5_check_remove(file_path_list, md5_list)
+            sys.exit()
+
+    print('Do you want to check the MD5 checksums? (y/n) (Default: y)')
+    md5 = input('MD5 check: ')
+    if md5.strip() == 'n':
+        md5 = False
+    else:
+        md5 = True
+
+    print('If the file already exists, do you want to overwrite it? (y/n) (Default: n)')
+    overwrite = input('Overwrite: ')
+    if overwrite.strip() == 'y':
+        overwrite = True
+    else:
+        overwrite = False
+
+    print('='*50)
+
+    # Download the files in the path "./Model/Experiment/Variable/..."
+    for index, row in file_list.iterrows():
+        # Create the path
+        path = os.path.join(root_path, row['Model'], row['Experiment'], row['Variable'])
+        # Create the path if it doesn't exist
+        if not os.path.exists(path):
+            os.makedirs(path)
+        # Download the file
+        file_md5 = row['fileMD5']
+        download_file(row['fileUrl'], path, file_md5=file_md5, md5_check=md5, overwrite=overwrite)
+        # Print the progress
+        print('Processed ' + str(index+1) + ' of ' + str(len(file_list)) + ' files')
+        print()
